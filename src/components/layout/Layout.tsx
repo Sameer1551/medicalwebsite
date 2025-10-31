1f506c680ccae950796c8f1f7935ae3113aaa959
Sameer1551 <siddheshvikram1@mail.com>
2025-10-31 14:25:33 +0530

    Initial commit: Medical website project

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import MobileNav from './MobileNav';
+import LanguageSwitcher from '../ui/LanguageSwitcher';
+
+const Layout: React.FC = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Navbar toggleSidebar={toggleSidebar} />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar isOpen={sidebarOpen} closeSidebar={() => setSidebarOpen(false)} />
+        <main className="flex-1 overflow-auto p-4 md:p-6 pb-20 md:pb-6">
+          <Outlet />
+        </main>
+      </div>
+      <div className="fixed bottom-4 right-4">
+        <LanguageSwitcher />
+      </div>
+      <MobileNav />
+    </div>
+  );
+};
+
+export default Layout;
